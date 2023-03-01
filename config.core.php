204cd9433f3a2eb83160281e77b848141d944df7
Bruno17 <b.perner@gmx.de>
2023-03-01 08:17:16 +0100

    prepare for GPM

---
+++ config.core.php
@@ -0,0 +1,2 @@
+<?php
+require(dirname(__DIR__, 2) . '/config.core.php');
--- core/components/siblingnav/elements/snippets/siblingnav.snippet.php
+++ core/components/siblingnav/elements/snippets/siblingnav.snippet.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * selfLink
+ * siblingNav
  * To navigate next, previous from the current resource or from the given resource
  * It can also show all or limited left/right and first/last siblings
  * Its also possible to navigate to childs of more than one parents 
@@ -26,17 +26,20 @@
  * sortOrder - 'ASC'
  * sortBy - menuindex
  * limit - false
- *
+ */
+ /*
  * Example usage:
- * [[!selfLink? &id=`[[*id]]` &direction=`next` &tpl=`commonName`]]
- *
+[[!siblingNav]]
+[[+sn.first]][[+sn.prev]][[+sn.prevlinks]][[+sn.self]][[+sn.nextlinks]][[+sn.next]][[+sn.last]]
+ */
+/*
  * @author Bruno Perner <b.perner@gmx.de>
  * @version 0.2-rc1
  */
 
 //$nav = $modx->getService('siblingnav','SiblingNav',$modx->getOption('siblingnav.core_path',null,$modx->getOption('core_path').'components/siblingnav/').'model/siblingnav/',$scriptProperties);
-
-include_once $modx->getOption('siblingnav.core_path',null,$modx->getOption('core_path').'components/siblingnav/model/siblingnav/').'siblingnav.class.php';
+//echo $modx->getOption('siblingnav.core_path',null,$modx->getOption('core_path').'components/siblingnav/').'model/siblingnav/siblingnav.class.php';
+include_once $modx->getOption('siblingnav.core_path',null,$modx->getOption('core_path').'components/siblingnav/').'model/siblingnav/siblingnav.class.php';
 $nav = new SiblingNav($modx,$scriptProperties);
 
 if (!($nav instanceof SiblingNav)) return '';
